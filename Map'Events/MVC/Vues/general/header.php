ebd257d14ee8c790d09d9fc1ee8452826559671a
c0sice <frappetimeo@gmail.com>
2026-06-01 16:57:30 +0200

    zddz

--- Map'Events/MVC/Vues/general/header.php
+++ Map'Events/MVC/Vues/general/header.php
@@ -6,6 +6,7 @@ if (basename($_SERVER["PHP_SELF"]) != "index.php")
 	header("Location:../index.php");
 	die("");
 }
+//asasa
 
 // Pose qq soucis avec certains serveurs...
 echo "<?xml version=\"1.0\" encoding=\"utf-8\" ?>";
